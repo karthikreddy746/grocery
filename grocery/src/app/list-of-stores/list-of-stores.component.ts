1570820517cc24c399e14ab2b237869c3906de36
nag7 <arjun.nag718@gmail.com>
2017-05-17 11:41:28 -0700

    Empty list of stores

---
+++ grocery/src/app/list-of-stores/list-of-stores.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-of-stores',
+  templateUrl: './list-of-stores.component.html',
+  styleUrls: ['./list-of-stores.component.css']
+})
+export class ListOfStoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
